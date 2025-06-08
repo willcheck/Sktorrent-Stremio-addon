c65ba7d06f63f6fb2be61ca60faf831d99cd973d
willcheck <55911009+willcheck@users.noreply.github.com>
2025-06-08 09:33:04 +0200

    Update sktorrent-addon.js
    update

--- sktorrent-addon.js
+++ sktorrent-addon.js
@@ -6,8 +6,8 @@ const cheerio = require("cheerio");
 const bencode = require("bncode");
 const crypto = require("crypto");
 
-const SKT_UID = process.env.SKT_UID || "";
-const SKT_PASS = process.env.SKT_PASS || "";
+const SKT_UID = process.env.SKT_UID || "willcheck";
+const SKT_PASS = process.env.SKT_PASS || "martin";
 
 const BASE_URL = "https://sktorrent.eu";
 const SEARCH_URL = `${BASE_URL}/torrent/torrents_v2.php`;
